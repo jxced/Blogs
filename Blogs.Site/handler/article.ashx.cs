4c565d86be252508464db38b804db213a2a2799a
djx <jxce_d@163.com>
2017-02-18 17:29:43 +0800

    study

---
+++ Blogs.Site/handler/article.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blogs.Site.handler
+{
+    /// <summary>
+    /// article 的摘要说明
+    /// </summary>
+    public class article : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            string sql=""
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
